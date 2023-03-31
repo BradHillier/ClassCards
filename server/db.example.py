22e24866946904bf80a260dabf11ef7b7c40ad66
Tyler Jacob Patton <pattontj@stumail.csci.viu.ca>
2023-03-31 11:03:56 -0700

    added example file

---
+++ server/db.example.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+
+def get_DB():
+    """Creates and returns a connection to the database
+    """
+    mdb = mysql.connector.connect(
+        host="",
+        user="",
+        password=""
+    )
+    return mdb
+
