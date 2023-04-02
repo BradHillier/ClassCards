cc400378421e8da3aee45bbe3602046ac27b0f7e
James Bradley Hillier (Brad) <hilliejb@stumail.csci.viu.ca>
2023-04-01 20:51:42 -0700

    add documention for the /api/cards/<int: cardID>/comments endpoint; changed 'Content' to 'content' to match the format of the other attributes

--- API/cards.py
+++ API/cards.py
@@ -6,19 +6,51 @@ card = Blueprint('card', __name__)
 
 @card.route('/api/cards/<int:cardID>/comments', methods=['GET', 'POST'])
 def card_comments(cardID: int):
+    """Handle actions related to comments on a specific card
 
+    Parameters:
+        cardID (int): The ID of the card to perform an action on
+
+    GET: get a list of comments for the specified card
+
+        Example usage:
+            curl http://127.0.0.1:5000/api/cards/1/comments
+
+        Returns:
+            a list containing a collection of comments in JSON format. Each card has the 
+            following fields: 
+
+                id      (int):  the unique identifier for the comment
+                content (str):  the message left on the comment by a user
+                cardID  (int):  the identifier for the card the comment was left on
+                userID  (int):  the identifier of the user who left the comment
+        
+    POST: add a comment to the specified card
+
+        Example usage:
+            curl http://127.0.0.1:5000/api/cards/1/comments -d '{
+                "content": "this card is great", 
+                "userID: 45
+            }'
+
+        Returns:
+            If the request was valid, a 201 status code and a json message indicating
+            that the  comment was successfully created. Otherwise, a 400 status code
+            along with a json error messaging indicating which portion of the request 
+            was invalid
+    """
     if request.method == 'GET':
         if cardID == 1:
             return [
                   {
                     'id': 1,
-                    'Content': 'this card is very good',
+                    'content': 'this card is very good',
                     'cardID': 1,
                     'userID': 45  
                   },
                   {
                     'id': 2,
-                    'Content': 'this card is very bad',
+                    'content': 'this card is very bad',
                     'cardID': 1,
                     'userID': 1
                   }
