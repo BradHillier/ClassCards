ffa0fc850027abb274d5bd380157ef4ce5d61f52
Tyler Jacob Patton <pattontj@stumail.csci.viu.ca>
2023-04-06 14:51:30 -0700

    create deck with cards in it [no tags]

--- API/decks.py
+++ API/decks.py
@@ -43,20 +43,20 @@ def get_decks():
 
         content = request.json
 
-        print(content)
+        #print(content)
 
         
         
         deck_name = content["name"]
-        print(deck_name)
+       # print(deck_name)
         is_public = content["isPublic"]
         author_id = content["authorID"]
         cards     = content["cards"]
 
         
-        print(is_public)
-        print(author_id)
-        print(cards)
+       # print(is_public)
+       # print(author_id)
+       # print(cards)
 
         # return {'message': 'test'}, 403
         
@@ -70,14 +70,14 @@ def get_decks():
         crs = dbs.cursor()
 
         # Prepare sql statement
-        sql = "INSERT (name, isPublic, ownerID) INTO Deck VALUES (?, ?, ?)"
+        sql = "INSERT INTO Deck  (name, isPublic, ownerID) VALUES (%s, %s, %s)"
         params = (deck_name, is_public, author_id,)
         
         try:
             crs.execute(sql, params)
             result = crs.fetchall()
         except mysql.connector.Error as err:
-            print(err)
+            print("Error at insert deck: ", err)
 
         # get the database ID of the deck for card inserts
         deck_id = crs.lastrowid
@@ -85,19 +85,30 @@ def get_decks():
         ins_cards = []
         card_tags = []
 
-        ins_query = "INSER INTO Card (front, back, isApproved, deckID) VALUES (?,?,?,?)"
+
+        future = []
+        
+        ins_query = "INSERT INTO Card (front, back, isApproved, deckID) VALUES (%s,%s,%s,%s)"
         
         # for each card, add to array and place tags in a tag array mapped to cardID
         try:
-            for f, b, t in cards:
+            for card in cards:
                 is_approved = True
 
+                f = card["front"]
+                b = card["back"]
+                tags = card["tags"]
+                
                 # insert front, back, approved, deckid) tuple into array of cards for
                 # one pass execution
                 ins_cards.append( (f, b, is_approved, deck_id ) )
 
-                # save card tags for another insert
-                card_tags.append(t)
+                
+                for tag in tags:
+                    # save card tags for another insert
+                    card_tags.append(tag)
+                    future.append( (f, b, tag,) )
+                    print(tag)
                 
         except mysql.connector.Error as err:
             print("Error: ", err)
@@ -108,11 +119,10 @@ def get_decks():
         except mysql.connector.Error as err:
             print("Error at insert every card: ", err)
 
+        '''
         # get last card ID from insert
         card_last_id = crs.lastrowid
         card_start_id = len(card_tags) - card_last_id
-
-
 
         # Map card ID to a tag set
         itr_start = card_start_id
@@ -122,40 +132,47 @@ def get_decks():
             for tag in tag_set:
                 cid_to_tags.append( (itr_start, tag) )   
             itr_start += 1
-            
         
-        ins_tags = []
 
-        # Collect all tags from different cards into one set
-        for tag_set in card_tags:
-            # Use set theory to merge tags without duplicates :>
-            ins_tags = list( set(ins_tags) | set(tag_set) )
-
+        formatted_tags = []
+        for tag in card_tags:
+            formatted_tags.append( (tag,) )
+        
         # MYSQL: insert ignore will ignore existing tags in DB
         ins_tags_query = "INSERT IGNORE INTO Tag (tag) VALUES (?)"
 
+
+        print(formatted_tags)
+        
         try:
-            crs.executemany(ins_tags_query, ins_tags)
+            crs.executemany(ins_tags_query, formatted_tags)
         except mysql.connector.Error as err:
             print("Error at insert tags: ", err)
 
+        # commit changes before next part
+        dbs.commit()
 
-    #Inserts a tag association using the id, and a select statement to find the ID of the tag
-    ins_tag_assoc = "INSERT INTO TagAssociation (cardID, tagID) VALUES (?, (SELECT Tag.ID FROM Tag WHERE Tag.tag = ? LIMIT 1))"
-
-    # TODO: Insert tag associations into table for each tag on a card 
-    try:
-        crs.executemany(ins_tag_assoc, cid_to_tags)
-    except mysql.connector.Error as err:
-        print("error at ins tag associations: ", err)
-    
-    # NOTE: do not uncomment until everything is working, otherwise partial
-    #       changes could be made to DB unintended
-        
-    # crs.commit()
+        qry = "INSERT INTO TagAssociation (cardID, tagID) VALUES ( (SELECT Card.ID FROM Card WHERE Card.front =%s AND Card.back=%s LIMIT 1), (SELECT Tag.ID FROM Tag WHERE Tag.tag =%s LIMIT 1) )"
             
-    # TODO: add ability to create a new deck
-    return {'message': 'successfully created a new deck'}, 201
+        #Inserts a tag association using the id, and a select statement to find the ID of the tag
+        ins_tag_assoc = "INSERT INTO TagAssociation (cardID, tagID) VALUES (%s, (SELECT Tag.ID FROM Tag WHERE Tag.tag = %s LIMIT 1))"
+
+        # TODO: Insert tag associations into table for each tag on a card 
+        try:
+            crs.executemany(qry, future)
+        except mysql.connector.Error as err:
+            print("error at ins tag associations: ", err)
+            
+        # NOTE: do not uncomment until everything is working, otherwise partial
+        #       changes could be made to DB unintended
+        '''    
+        dbs.commit()
+            
+        crs.close()
+        dbs.close()
+    
+        # TODO: add ability to create a new deck
+        return {'message': 'successfully created a new deck'}, 201
 
 
 @deck.route('/api/decks/<int:deckID>/cards', methods=['GET', 'POST'])
