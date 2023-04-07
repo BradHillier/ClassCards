f77c9f1de1f6fe80e1d982552ee3b2aa3a3439ea
Johann Cascante Suarez <suarezjc@stumail.csci.viu.ca>
2023-04-07 10:44:58 -0700

    Addded card rendering feature

--- frontend/static/js/old_view_decks.js
+++ frontend/static/js/old_view_decks.js
@@ -7,13 +7,42 @@ fetch("/api/decks")
   .then((data) => {
     const deckCards = data.decks.map((deck) => {
       return `
-        <div class="deck">
+        <div class="deck" data-deck-id="${deck.id}">
           <div class="deck-body">
-            <h5 class="deck-title"><a href="/api/decks/${deck.id}/cards">${deck.title}</a></h5>
+            <h5 class="deck-title"><a href="#">${deck.title}</a></h5>
           </div>
         </div>
       `;
     });
 
     deckContainer.innerHTML = deckCards.join("");
+
+    // add event listeners to the deck elements
+    const decks = document.querySelectorAll(".deck");
+    decks.forEach((deck) => {
+      deck.addEventListener("click", () => {
+        const deckId = deck.dataset.deckId;
+        fetch(`/api/decks/${deckId}/cards`)
+          .then((response) => response.json())
+          .then((cards) => {
+            const cardItems = cards.map((card) => {
+              return `
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">${card.front}</h5>
+                    <p class="card-text">${card.back}</p>
+                  </div>
+                </div>
+              `;
+            });
+            deckContainer.innerHTML = cardItems.join("");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    });
+  })
+  .catch((error) => {
+    console.error(error);
   });
